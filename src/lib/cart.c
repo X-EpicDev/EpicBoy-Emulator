381cbe4d4475de819ade6cd38c30876a80c74c53
Aidan Smith <xepicdev.business@gmail.com>
2025-04-29 16:37:01 +1000

    added ROM Types

--- src/lib/cart.c
+++ src/lib/cart.c
@@ -11,9 +11,42 @@ typedef struct {
 
 static cartContext ctx;
 
-//will add the remaining rom types soon
 static const char *RomTypes[] = {
-    "ROM ONLY"
+    "ROM ONLY",
+    "MBC1",
+    "MBC1 + RAM",
+    "MBC1 + RAM + BATTERY",
+    "0x04 INVALID",
+    "MBC2",
+    "MBC2 + BATTERY",
+    "0x07 INVALID",
+    "ROM + RAM 1",
+    "ROM + RAM + BATTERY 1",
+    "0x0A INVALID",
+    "MMM01",
+    "MMM01 + RAM",
+    "MMM01 + RAM + BATTERY",
+    "0x0E INVALID",
+    "MBC3 + TIMER + BATTERY",
+    "MBC3 + TIMER + RAM + BATTERY 2",
+    "MBC3",
+    "MBC3 + RAM 2",
+    "MBC3 + RAM + BATTERY 2",
+    "0x14 INVALID",
+    "0x15 INVALID",
+    "0x16 INVALID",
+    "0x17 INVALID",
+    "0x18 INVALID",
+    "MBC5",
+    "MBC5 + RAM",
+    "MBC5 + RAM + BATTERY",
+    "MBC5 + RUMBLE",
+    "MBC5 + RUMBLE + RAM",
+    "MBC5 + RUMBLE + RAM + BATTERY",
+    "0x1F INVALID",
+    "MBC6",
+    "0x21 INVALID",
+    "MBC7 + SENSOR + RUMBLE + RAM + BATTERY"
 };
 
 //will add the remaining license codes soon
